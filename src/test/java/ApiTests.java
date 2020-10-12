ea2fbe9e40b6390468ce232db1eb5f123e985228
jaroslaw kotus <jaroslaw.kotus@testarmy.com>
2020-10-12 11:31:50 +0200

    Backend tests

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -1,41 +1,47 @@
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import groovy.json.JsonBuilder;
+
+import com.github.javafaker.Faker;
+import com.jayway.restassured.response.Response;
 import org.json.JSONObject;
 import org.junit.Test;
-import utils.ResponseClass;
+import utils.ApiClient;
 
 import java.util.List;
-import java.util.Random;
+import java.util.logging.Logger;
 
 
 public class ApiTests {
 
-    ResponseClass response;
-
+    private final static Logger LOGGER = Logger.getLogger(ApiTests.class.getName());
+    Faker faker = new Faker();
 
     @Test
     public void getMaxValueForUserId() {
-        List<Integer> jsonResponse = ResponseClass.doGetRequest("https://jsonplaceholder.typicode.com/posts/",200).jsonPath().getList("userId");
-          int max=  jsonResponse.stream().max(Integer::compareTo).get();
-        System.out.println(max);
+        List<Integer> jsonResponse = ApiClient.doGetRequest("https://jsonplaceholder.typicode.com/posts/", 200).jsonPath().getList("userId");
+        int max = jsonResponse.stream().max(Integer::compareTo).get();
+        LOGGER.info(String.valueOf(max));
     }
 
     @Test
     public void getMaxValueForId() {
-        List<Integer> jsonResponse = ResponseClass.doGetRequest("https://jsonplaceholder.typicode.com/posts?userId=10", 200).jsonPath().getList("id");
-        System.out.println(jsonResponse);
-       int max = jsonResponse.stream().max(Integer::compareTo).get();
+        List<Integer> jsonResponse = ApiClient.doGetRequest("https://jsonplaceholder.typicode.com/posts?userId=10", 200).jsonPath().getList("id");
+        int max = jsonResponse.stream().max(Integer::compareTo).get();
+        LOGGER.info(String.valueOf(max));
     }
 
     @Test
-    public void post() {
-// tworzenie json///////////////
-        JSONObject j = new JSONObject();
-        j.put("postId",10);
-////////////////
+    public void addNewCommentForPostId() {
+        List<Integer> jsonResponse = ApiClient.doGetRequest("https://jsonplaceholder.typicode.com/posts?userId=10", 200).jsonPath().getList("id");
+        int max = jsonResponse.stream().max(Integer::compareTo).get();
+        LOGGER.info(String.valueOf(max));
 
-        System.out.println( ResponseClass.doPostRequest("https://jsonplaceholder.typicode.com/comments",
-              j.toString(),201).jsonPath().get().toString());
+        JSONObject json = new JSONObject();
+        json.put("postId", max);
+        json.put("name", faker.name().fullName());
+        json.put("email", faker.name().username() + "@gmail.com");
+        json.put("body", faker.chuckNorris().fact());
+
+        Response res = ApiClient.doPostRequest("https://jsonplaceholder.typicode.com/comments",
+                json.toString(), 201);
+        LOGGER.info(res.jsonPath().getString("$"));
     }
 }
